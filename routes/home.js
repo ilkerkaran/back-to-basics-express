32308db863bc9f49d037f5afba1bea5b53061247
ilkerkaran <ilkerkaran@gmail.com>
2020-06-02 18:16:47 +0100

    Home style and structer added

--- routes/home.js
+++ routes/home.js
@@ -3,5 +3,5 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send({ message: 'Welcome Home' }));
+router.get('/', (req, res) => res.render('home'));
 module.exports = router;
