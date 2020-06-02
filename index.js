a0d029b6b1268a9a49a1c88257914d872e8a92dd
ilkerkaran <ilkerkaran@gmail.com>
2020-06-02 16:45:56 +0100

    routes static files and body parser middlewares registered

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const router = require('./routes');
+const bodyParser = require('body-parser');
+
+
 const app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public'));
+app.use('/', router);
+
 
 app.listen(5000, () => 'App started on 5000');
